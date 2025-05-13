eee7eb4d5cf92f3a2c9f656cf5d214851d8463fe
Sean Frett <smf0x10dev@gmail.com>
2025-05-13 14:18:10 -0500

    added KLM train script

---
+++ train-belle-2-klm-mmdet.sh
@@ -0,0 +1,3 @@
+# This is set up to use 4 GPUs; to only use 1, change CUDA_VISIBLE_DEVICES to 0 and replace the 4 on the end with a 1
+cd mmdetection
+CUDA_VISIBLE_DEVICES=0 ./tools/dist_train.sh ./configs/belle_2/belle-2-klm-hep2coco-mmt-simple.py 1
